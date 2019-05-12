74645b18e0d8b9cd0d3d6892b77ec0dc5cdf4cdc
yinkeet <wongyinkeet@gmail.com>
2019-05-12 20:52:24 +0800

    Changed name

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='sanic-validator',
+    name='chimera',
     version='0.0.1',
-    description='Validators for use with sanic',
-    url='git@github.com:yinkeet/SanicValidator.git',
+    description='A collection of helpers',
+    url='git@github.com:yinkeet/chimera.git',
     author='Wong Yin Keet',
     author_email='yinkeet@gmail.com',
     license='unlicense',
-    packages=['SanicValidator'],
+    packages=['chimera'],
     zip_safe=False,
     install_requires = [
         'cerebrus==1.3',
