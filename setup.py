0a03feac2a06d7a87a53908ab8b13ee5d62f2d8b
Keet <keet@socomy.com>
2019-05-12 17:00:14 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sanic-validator',
+    version='0.0.1',
+    description='Validators for use with sanic',
+    url='git@github.com:yinkeet/sanic-validator.git',
+    author='Wong Yin Keet',
+    author_email='yinkeet@gmail.com',
+    license='unlicense',
+    packages=['sanic-validator'],
+    zip_safe=False
+)
