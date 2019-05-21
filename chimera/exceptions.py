840931365bfec2eff93ca8290a8a59d496f728b5
yinkeet <wongyinkeet@gmail.com>
2019-05-21 17:08:11 +0800

    Added DocumentNotFound helper exception

--- chimera/exceptions.py
+++ chimera/exceptions.py
@@ -1,5 +1,16 @@
-from sanic.exceptions import add_status_code, SanicException
+from sanic.exceptions import add_status_code, SanicException, NotFound
 
 @add_status_code(409)
 class Conflict(SanicException):
     pass
+
+class DocumentNotFound(NotFound):
+    def __init__(self, field, value, document_name='document', extra=None):
+        messages = ['{} \'{}\' not found'.format(document_name.capitalize(), value)]
+        if isinstance(extra, str):
+            messages.append(extra)
+        if isinstance(extra, list):
+            messages.extend(extra)
+        super().__init__({
+            field: messages
+        })
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='chimera',
-    version='0.0.3',
+    version='0.0.4',
     description='A collection of helpers',
     url='git@github.com:yinkeet/chimera.git',
     author='Wong Yin Keet',
