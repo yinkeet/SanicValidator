a3b321b438e0b797742e48bf320aa50a49cd5126
yinkeet <wongyinkeet@gmail.com>
2019-07-26 09:10:31 +0800

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='chimera',
-    version='0.0.5',
+    version='0.1.0',
     description='A collection of helpers',
     url='git@github.com:yinkeet/chimera.git',
     author='Wong Yin Keet',
