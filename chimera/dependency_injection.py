2160c5cb40ebd3ba6314e2261baff2684bf23c11
yinkeet <wongyinkeet@gmail.com>
2019-07-26 09:38:09 +0800

    Added shorcut load method

--- chimera/dependency_injection.py
+++ chimera/dependency_injection.py
@@ -6,6 +6,10 @@ from inspect import signature
 from sanic.log import logger
 
 class Dependencies(object):
+    @staticmethod
+    def load(app):
+        return app.dependencies
+
     def __init__(self, app, loop):
         self.app = app
         self.loop = loop
