89f631a9b7cca28c75fd93d136b1ee6c2d95934b
yinkeet <wongyinkeet@gmail.com>
2019-10-04 10:18:36 +0800

    Added oauth2 authenticate

---
+++ chimera/oauth2.py
@@ -0,0 +1,19 @@
+from functools import wraps
+
+from sanic.exceptions import Forbidden
+
+
+class Authenticate(object):
+    def __init__(self, allowed_scopes: list):
+        self.allowed_scopes = allowed_scopes
+
+    def __call__(self, function):
+        @wraps(function)
+        async def wrapper(request, *args, **kwargs):
+            scopes = request.headers.get('scope', '').split()
+            allowed = any(scope in scopes  for scope in self.allowed_scopes)
+            if not allowed:
+                raise Forbidden(['Insufficient scope: authorized scope is insufficient'])
+            return await function(request, *args, **kwargs)
+
+        return wrapper
