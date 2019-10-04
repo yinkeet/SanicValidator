841f8c20b98d7375b2cae1327db44719b55328e2
yinkeet <wongyinkeet@gmail.com>
2019-10-04 17:42:51 +0800

    Fixed an error in the new validate magic method

--- chimera/validator.py
+++ chimera/validator.py
@@ -146,16 +146,15 @@ class Validate(object):
                 validator = CustomValidator(request=request, schema=self.schema, allow_unknown=True)
                 if not validator.validate(kwargs):
                     raise InvalidUsage(validator.errors)
+                return await function(request, *args, **validator.document)
             else:
                 validator = CustomValidator(request=request, schema=self.schema, purge_unknown=True)
                 document = getattr(request, self.request_property, {})
                 document = dict(document) if document else {}
                 if not validator.validate(document):
                     raise InvalidUsage(validator.errors)
-
-            if self.request_property in signature(function).parameters:
-                kwargs[self.request_property] = validator.document 
-
-            return await function(request, *args, **kwargs)
+                if self.request_property in signature(function).parameters:
+                    kwargs[self.request_property] = validator.document
+                return await function(request, *args, **kwargs)
 
         return wrapper
