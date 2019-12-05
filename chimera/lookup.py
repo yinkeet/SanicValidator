84ed4451dfca8e4c5bd162fb43da7657883190fe
yinkeet <wongyinkeet@gmail.com>
2019-12-05 10:45:49 +0800

    Added post lookup

--- chimera/lookup.py
+++ chimera/lookup.py
@@ -4,7 +4,7 @@ from functools import partial
 from sanic.log import logger
 from sanic.exceptions import ServerError
 
-from requests import get
+from requests import get, post
 
 
 async def lookup(url, params, return_id):
@@ -16,6 +16,25 @@ async def lookup(url, params, return_id):
     if response.status_code != 200:
         logger.error(response.text)
         raise ServerError(['Lookup has encountered an error', url, params])
+
+    results = response.json()['results']
+    if len(results):
+        results = response.json()['results'][0]
+
+    return {
+        'id': return_id,
+        'results': results
+    }
+
+async def post(url, json, return_id):
+    response = await get_event_loop().run_in_executor(None, partial(post, 
+        url=url,
+        json=json
+    ))
+
+    if response.status_code != 200:
+        logger.error(response.text)
+        raise ServerError(['Lookup has encountered an error', url, json])
 
     results = response.json()['results']
     if len(results):
