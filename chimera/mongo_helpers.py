08079ae510c94696702257f95278b151fc44ea66
yinkeet <wongyinkeet@gmail.com>
2019-12-13 17:28:11 +0800

    Added custom_aggregate

--- chimera/mongo_helpers.py
+++ chimera/mongo_helpers.py
@@ -1,3 +1,5 @@
+from sanic.log import logger
+
 def create_sort_query(fields):
     """Creates a sort query for mongo aggregation"""
     results=[]
@@ -10,3 +12,12 @@ def create_sort_query(fields):
             results.append(field)
 
     return { (field.lower()):(1 if field.isupper() else -1) for field in results }
+
+async def custom_aggregate(collection, query, function_name=None):
+    if function_name:
+        logger.debug('%s query %s', function_name, query)
+    
+    output = (await collection.aggregate(query).fetch_next).next_object()
+
+    if function_name:
+        logger.debug('%s response %s', function_name, output)
