800a098024874f98314a2fc43e273233c7afcd47
yinkeet <wongyinkeet@gmail.com>
2019-12-13 18:12:59 +0800

    Remove response from returning false

--- chimera/rest_helpers.py
+++ chimera/rest_helpers.py
@@ -2,7 +2,4 @@ def standardize_response(namespace, response):
     if isinstance(response, list):
         return {namespace: response}
 
-    if not response:
-        return False
-
     return {namespace: [response]}
