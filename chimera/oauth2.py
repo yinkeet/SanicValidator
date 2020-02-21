cbfb5411529af9191ecdf95e988a07ed6f585264
yinkeet <wongyinkeet@gmail.com>
2020-02-21 14:06:55 +0800

    Added roles support

--- chimera/oauth2.py
+++ chimera/oauth2.py
@@ -5,8 +5,9 @@ from sanic.exceptions import Forbidden
 
 
 class Authenticate(object):
-    def __init__(self, allowed_scopes: list=[]):
+    def __init__(self, allowed_scopes: list=[], allowed_roles: list=[]):
         self.allowed_scopes = allowed_scopes
+        self.allowed_roles = allowed_roles
 
     def __call__(self, function):
         @wraps(function)
@@ -14,10 +15,15 @@ class Authenticate(object):
             if self.allowed_scopes:
                 headers = dict(getattr(request, 'headers', {}))
                 scopes = headers.get('scope', '').split()
-                allowed = any(scope in scopes  for scope in self.allowed_scopes)
+                allowed = any(scope in scopes for scope in self.allowed_scopes)
                 if not allowed:
                     raise Forbidden(['Insufficient scope: authorized scope is insufficient'])
-            
+            if self.allowed_roles:
+                headers = dict(getattr(request, 'headers', {}))
+                roles = headers.get('roles', '').split()
+                allowed = any(role in roles for role in self.allowed_roles)
+                if not allowed:
+                    raise Forbidden(['Insufficient role: authorized role is insufficient'])
             return await function(request, *args, **kwargs)
 
         return wrapper
