094e1ab93041d4c7b85ad48f12866829ed2e1fc7
yinkeet <wongyinkeet@gmail.com>
2020-02-25 15:35:32 +0800

    Added crc32 and crc32s

--- chimera/hasher.py
+++ chimera/hasher.py
@@ -6,7 +6,10 @@ from zlib import crc32 as zlib_crc32
 def md5(objects):
     return hashlib_md5(dumps(objects, sort_keys=True).encode('utf-8')).hexdigest()
 
-def crc32(json_string: str):
-    dictionary = loads(json_string)
+def crc32(dictionary: dict):
     sorted_encoded_json_string = dumps(dictionary, sort_keys=True).encode('utf-8')
     return zlib_crc32(sorted_encoded_json_string)
+
+def crc32s(json_string: str):
+    dictionary = loads(json_string)
+    return crc32(dictionary)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='chimera',
-    version='0.1.31',
+    version='0.1.32',
     description='A collection of helpers',
     url='git@github.com:yinkeet/chimera.git',
     author='Wong Yin Keet',
