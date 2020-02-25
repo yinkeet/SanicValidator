c69b76b630da76d4d42e4f872d5315aa0d5936d5
yinkeet <wongyinkeet@gmail.com>
2020-02-25 13:59:40 +0800

    Added crc32

--- chimera/hasher.py
+++ chimera/hasher.py
@@ -1,5 +1,12 @@
-from hashlib import md5 as md5_hasher
-from json import dumps
+from hashlib import md5 as hashlib_md5
+from json import dumps, loads
+from zlib import crc32 as zlib_crc32
+
 
 def md5(objects):
-    return md5_hasher(dumps(objects, sort_keys=True).encode('utf-8')).hexdigest()
+    return hashlib_md5(dumps(objects, sort_keys=True).encode('utf-8')).hexdigest()
+
+def crc32(json_string: str):
+    dictionary = loads(json_string)
+    sorted_encoded_json_string = dumps(dictionary, sort_keys=True).encode('utf-8')
+    return zlib_crc32(sorted_encoded_json_string.encode('utf-8'))
