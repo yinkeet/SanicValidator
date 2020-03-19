7d12a8e39b679ad2018a2e1c14993f0f4f13ac12
yinkeet <wongyinkeet@gmail.com>
2020-03-20 01:05:13 +0800

    Added validator for testing use

--- chimera/test_helpers.py
+++ chimera/test_helpers.py
@@ -1,3 +1,9 @@
+from datetime import datetime
+
+from bson import ObjectId
+from cerberus import Validator as BaseValidator
+
+
 class MockResponse:
     def __init__(self, status_code: int, json_response: dict):
         self.status_code = status_code
@@ -9,3 +15,11 @@ class MockResponse:
 
     def json(self, **kwargs):
         return self.json_response
+
+class Validator(BaseValidator):
+    # Types
+    def _validate_type_object_id(self, value):
+        return isinstance(value, ObjectId)
+
+    def _validate_type_datetime(self, value):
+        return isinstance(value, datetime)
