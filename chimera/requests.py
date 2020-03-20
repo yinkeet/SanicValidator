e81397311387b6b94e87a268c01ecbd3e4b5c832
yinkeet <wongyinkeet@gmail.com>
2020-03-20 12:04:31 +0800

    Used import instead of from so that monkeypatching on pytest works

--- chimera/requests.py
+++ chimera/requests.py
@@ -1,8 +1,8 @@
 from asyncio import get_event_loop
 from functools import partial
 
-from requests import request as _request
+import requests
 
 
 async def request(method, url, **kwargs):
-    return await get_event_loop().run_in_executor(None, partial(_request, method, url, **kwargs))
+    return await get_event_loop().run_in_executor(None, partial(requests.request, method, url, **kwargs))
