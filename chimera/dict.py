a01aa70b81e9ffa0bff5273e903465c0ed5a9dd7
yinkeet <wongyinkeet@gmail.com>
2020-09-10 13:05:30 +0800

    Fixed a warning

--- chimera/dict.py
+++ chimera/dict.py
@@ -1,10 +1,10 @@
-import collections
+from collections import MutableMapping
 
 def flatten(d, parent_key='', seperator='_'):
     items = []
     for k, v in d.items():
         new_key = parent_key + seperator + k if parent_key else k
-        if isinstance(v, collections.MutableMapping):
+        if isinstance(v, MutableMapping):
             items.extend(flatten(v, new_key, seperator=seperator).items())
         else:
             items.append((new_key, v))
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='chimera',
-    version='0.1.67',
+    version='0.1.68',
     description='A collection of helpers',
     url='git@github.com:yinkeet/chimera.git',
     author='Wong Yin Keet',
